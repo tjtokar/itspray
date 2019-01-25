c00b1a7cc10bf57dfe6acd6ca48ef80e4decff18
TJ Tokarczyk <t.tokarczyk09@gmail.com>
2019-01-25 16:36:21 -0500

    Second Commit

--- home.php
+++ home.php
@@ -2,16 +2,13 @@
 <?php get_header(); ?>
     <?php while ( have_posts() ) : the_post(); ?>
       <?php
-        // check if the flexible content field has rows of data
         if( have_rows('home') ):
-             // loop through the rows of data
             while ( have_rows('home') ) : the_row();
                  get_template_part( 'flexible-templates/'. get_row_layout() );
             endwhile;
         else :
-            // no layouts found
         endif;
         ?>
-    <?php endwhile; // end of the loop. ?>
-    
+    <?php endwhile;  ?>
+
 <?php get_footer(); ?>
