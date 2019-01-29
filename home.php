788eb66a021e7f726080d106d26cb862553bdc5d
TJ Tokarczyk <t.tokarczyk09@gmail.com>
2019-01-29 14:52:18 -0500

    fix summary

--- home.php
+++ home.php
@@ -4,8 +4,7 @@
       <div class="container h-100">
         <div class="row h-100 d-flex">
           <div class="col-6 offset-3 text-center align-self-center">
-            <?xml version="1.0" encoding="utf-8"?>
-<!-- Generator: Adobe Illustrator 23.0.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+          
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 1000 1000" style="enable-background:new 0 0 1000 1000;" xml:space="preserve">
    <defs>
